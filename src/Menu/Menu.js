02c5fd2ade5d88d996397ad8d102a5c2d294d950
sgali31 <sgali3@uncc.edu>
2025-02-23 20:28:30 -0500

    Changes in scss

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -8,7 +8,7 @@ function Menu() {
         <li><Link to="/" aria-current="page">Homepage</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/login">Login</Link></li>
-        {/* Use <a> for external links instead of <Link> */}
+        {}
         <li><a href="https://google.com" target="_blank" rel="noopener noreferrer">Google</a></li>
       </ul>
     </nav>
